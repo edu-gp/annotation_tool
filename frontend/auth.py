41426fcc6f86bd4094cd9add162686e00395a7ec
Eddie Du <edu@georgianpartners.com>
2020-02-25 10:02:36 -0500

    allow users to "log in" without password

--- frontend/auth.py
+++ frontend/auth.py
@@ -8,38 +8,39 @@ from flask import (
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-# TODO
-DUMMY_USER = {
-    'id': 1,
-    'username': 'eddie'
-}
-
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
         username = request.form['username']
-        password = request.form['password']
+        # password = request.form['password']
         # db = get_db()
         # error = None
         # user = db.execute(
         #     'SELECT * FROM user WHERE username = ?', (username,)
         # ).fetchone()
 
-        # TODO
-        error = None
-        user = DUMMY_USER
+        # # TODO
+        # error = None
+        # user = DUMMY_USER
         
         # if user is None:
         #     error = 'Incorrect username.'
         # elif not check_password_hash(user['password'], password):
         #     error = 'Incorrect password.'
 
-        if error is None:
-            session.clear()
-            session['user_id'] = user['id'] # TODO
-            return redirect(url_for('index'))
+        # if error is None:
+        #     session.clear()
+        #     session['user_id'] = user['id'] # TODO
+        #     return redirect(url_for('index'))
 
-        flash(error)
+        # flash(error)
+
+        # NOTE: Temporary way to sign in users.
+        session.clear()
+        session['user_id'] = username
+        session['username'] = username
+
+        return redirect(url_for('index'))
 
     return render_template('auth/login.html')
 
@@ -50,15 +51,22 @@ def logout():
 
 @bp.before_app_request
 def load_logged_in_user():
-    user_id = session.get('user_id')
+    # user_id = session.get('user_id')
 
-    if user_id is None:
+    # if user_id is None:
+    #     g.user = None
+    # else:
+    #     g.user = get_db().execute(
+    #         'SELECT * FROM user WHERE id = ?', (user_id,)
+    #     ).fetchone()
+
+    if session.get('user_id') is None:
         g.user = None
     else:
-        g.user = DUMMY_USER
-        # g.user = get_db().execute(
-        #     'SELECT * FROM user WHERE id = ?', (user_id,)
-        # ).fetchone()
+        g.user = {
+            'user_id': session.get('user_id'),
+            'username': str(session.get('username'))
+        }
 
 def login_required(view):
     @functools.wraps(view)
