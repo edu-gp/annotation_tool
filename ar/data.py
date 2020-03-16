333acb342a994addcd1b8269ea281a75ce8d033f
Eddie Du <sj.eddie@gmail.com>
2020-03-16 14:57:48 -0400

    refactor and add test to

--- ar/data.py
+++ ar/data.py
@@ -233,38 +233,70 @@ def _majority_label(labels):
     else:
         return None
 
-def export_labeled_examples(task_id, outfile=None):
-    # TODO Current interannotator agreement is majority vote.
-    # See Snorkel for some inspiration for the future.
-    
-    # labels will start off being:
-    # {
-    #   'ar_id_12345': {
-    #     'HEALTHCARE': [1, 1, -1, 1, 0, 0]
-    #   }
-    # }
-    # Then merged into:
-    # {
-    #   'ar_id_12345': {
-    #     'HEALTHCARE': 1
-    #   }
-    # }
-    # Finally we merge labels with text into:
-    # {'text': '...', 'labels': {'HEALTHCARE': 1}}
+def _export_labeled_examples(annotations_iterator):
+    """
+    Inputs:
+        annotations_iterator: A iterator that returns annotations.
 
-    labels = defaultdict(lambda: defaultdict(list))
+    An example annotation looks like:
+        {
+            'req': {
+                'ar_id': '...'
+                'data': {
+                    'text': '...'
+                }
+            },
+            'anno': {
+                'labels': {
+                    'HEALTHCARE': 1,
+                    'POP_HEALTH': -1,
+                    'AI': 0,
+                }
+            }
+        }
+
+    Step 1. Labels will start off being gathered for each ar_id:
+    {
+        'ar_id_12345': {
+            'HEALTHCARE': [1, 1, -1, 1, 0, 0]
+        },
+        ...
+    }
+
+    Step 2. Then they're merged by a merging strategy (currently majority vote):
+    {
+        'ar_id_12345': {
+            'HEALTHCARE': 1
+        },
+        ...
+    }
+
+    Step 3. Finally we join labels with the text using ar_id.
+    [
+        {
+            'text': '...',
+            'labels': {'HEALTHCARE': 1}
+        },
+        ...
+    ]
+
+    This function returns result from the last step.
+    """
+
     text = {}
+    labels = defaultdict(lambda: defaultdict(list))
 
-    for user_id in _get_all_annotators_from_annotated(task_id):
-        for ar_id in fetch_all_annotations(task_id, user_id):
-            anno = fetch_annotation(task_id, user_id, ar_id)
-            text[ar_id] = anno['req']['data']['text']
-            for label_key, label_value in anno['anno']['labels'].items():
-                labels[ar_id][label_key].append(label_value)
+    # Step 1. Gather all the labels (and text)
+    for anno in annotations_iterator:
+        ar_id = anno['req']['ar_id']
 
-    # print(text)
-    # print(labels)
+        text[ar_id] = anno['req']['data'].get('text') or ''
 
+        for label_key, label_value in anno['anno']['labels'].items():
+            # e.g. label_key = 'HEALTHCARE', label_value = 1
+            labels[ar_id][label_key].append(label_value)
+
+    # Step 2. Merge all labels within each ar_id
     new_labels = {}
     for ar_id in labels:
         new_labels[ar_id] = {
@@ -274,18 +306,30 @@ def export_labeled_examples(task_id, outfile=None):
         }
     labels = new_labels
 
-    # print(new_labels)
-
+    # Step 3. Join with text on ar_id
     final = []
-
     for ar_id in labels:
+        # If any labels are left - An example could have the "unsure" label for
+        # all its annotations, and since we remove all unsure labels, there
+        # might be any labels left.
         if len(labels[ar_id]) > 0:
             final.append({
                 'text': text[ar_id],
                 'labels': labels[ar_id]
             })
 
-    # print(final)
+    return final
+
+
+def export_labeled_examples(task_id, outfile=None):
+    def annotations_iterator():
+        for user_id in _get_all_annotators_from_annotated(task_id):
+            for ar_id in fetch_all_annotations(task_id, user_id):
+                anno = fetch_annotation(task_id, user_id, ar_id)
+
+                yield anno
+
+    final = _export_labeled_examples(annotations_iterator())
 
     if outfile is not None:
         save_jsonl(outfile, final)
---
+++ tests/unit/test_export_labeled_examples.py
@@ -0,0 +1,104 @@
+from ar.data import _export_labeled_examples
+
+def test__export_labeled_examples__simple():
+    AR_1 = {
+        'ar_id': 'foo_1',
+        'data': {'text': 'sometext_1'}
+    }
+    AR_2 = {
+        'ar_id': 'foo_2',
+        'data': {'text': 'sometext_2'}
+    }
+
+    sample_annotations = [
+        # User A annotates AR_1
+        {
+            'req': AR_1,
+            'anno': {
+                'labels': {
+                    'HEALTHCARE': 1,
+                    'POP_HEALTH': -1,
+
+                    # This one will be dropped since user is unsure
+                    'AI': 0,
+                }
+            }
+        },
+        # User B annotates AR_1
+        {
+            'req': AR_1,
+            'anno': {
+                'labels': {
+                    'HEALTHCARE': 1,
+                }
+            }
+        },
+        # User C annotates AR_1
+        {
+            'req': AR_1,
+            'anno': {
+                'labels': {
+                    # User C disagrees with User A and B, but they're the majority.
+                    # So HEALTHCARE would be 1.
+                    'HEALTHCARE': -1,
+                }
+            }
+        },
+        # User C annotates AR_2
+        {
+            'req': AR_2,
+            'anno': {
+                'labels': {
+                    'HEALTHCARE': -1,
+                }
+            }
+        },
+    ]
+
+    result = _export_labeled_examples(sample_annotations)
+    assert result == [
+        {'text': 'sometext_1', 'labels': {'HEALTHCARE': 1, 'POP_HEALTH': -1}},
+        {'text': 'sometext_2', 'labels': {'HEALTHCARE': -1}}
+    ]
+
+
+def test__export_labeled_examples__empty_text():
+    """Currently we allow empty or missing text"""
+    AR_1 = {
+        'ar_id': 'bad_1',
+        'data': {'text': None}
+    }
+    AR_2 = {
+        'ar_id': 'bad_2',
+        'data': {}
+    }
+
+    sample_annotations = [
+        # User A annotates AR_1
+        {
+            'req': AR_1,
+            'anno': {
+                'labels': {
+                    'HEALTHCARE': 1,
+                }
+            }
+        },
+        # User B annotates AR_1
+        {
+            'req': AR_2,
+            'anno': {
+                'labels': {
+                    'HEALTHCARE': 1,
+                }
+            }
+        },
+    ]
+
+    result = _export_labeled_examples(sample_annotations)
+
+    # Note the behaviour for missing text is not ideal, but this is the
+    # 'official' behaviour for now.
+    assert result == [
+        {'text': '', 'labels': {'HEALTHCARE': 1}},
+        {'text': '', 'labels': {'HEALTHCARE': 1}}
+    ]
