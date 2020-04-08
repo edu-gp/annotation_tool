a17ea1b96b7730d9428abfadfce5773dca13a1ab
Eddie Du <sj.eddie@gmail.com>
2020-04-08 13:19:14 -0400

    InferenceResults bug fix introduced recently

--- train/no_deps/inference_results.py
+++ train/no_deps/inference_results.py
@@ -8,10 +8,12 @@ class InferenceResults:
         self.probs = raw_to_pos_prob(self.raw)
 
     def save(self, inf_fname):
-        save(inf_fname + '.npy', self.raw)
+        if not inf_fname.endswith('.npy'):
+            inf_fname = inf_fname + '.npy'
+        save(inf_fname, self.raw)
 
     @staticmethod
     def load(inf_fname):
-        if not 'inf_fname'.endswith('.npy'):
-            inf_fname + '.npy'
+        if not inf_fname.endswith('.npy'):
+            inf_fname = inf_fname + '.npy'
         return InferenceResults(load(inf_fname, allow_pickle=True))
