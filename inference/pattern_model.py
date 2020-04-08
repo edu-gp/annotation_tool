b52c13f10f103dd33409b509d4386906d769198d
Jing Zhang <jzhang@georgianpartners.com>
2020-04-08 15:42:37 -0400

    Change as discussed

--- inference/pattern_model.py
+++ inference/pattern_model.py
@@ -14,15 +14,15 @@ def _compare_matches(item1, item2):
     :return: comparison result where -1 means item1 < item2, 1 means item1 >
     item2 and 0 means item1 == item2
     """
-    if (item1[2] - item1[1]) < (item2[2] - item2[1]):
+    if item1[1] < item2[1]:
         return -1
-    elif (item1[2] - item1[1]) > (item2[2] - item2[1]):
+    elif item1[1] > item2[1]:
         return 1
     else:
-        return _compare_last_matching_index(item1, item2)
+        return _compare_match_length(item1, item2)
 
 
-def _compare_last_matching_index(item1, item2):
+def _compare_match_length(item1, item2):
     """Compare two matches from the Pattern Matcher by the last matching index.
 
     :param item1: first matches item
@@ -30,12 +30,41 @@ def _compare_last_matching_index(item1, item2):
     :return: comparison result where -1 means item1 < item2, 1 means item1 >
     item2 and 0 means item1 == item2
     """
-    if item1[2] < item2[2]:
+    if (item1[2] - item1[1]) < (item2[2] - item2[1]):
         return -1
-    elif item1[2] > item2[2]:
+    elif (item1[2] - item1[1]) > (item2[2] - item2[1]):
         return 1
     else:
         return 0
+
+
+def _maximize_non_overlapping_matches(matches):
+    # All matches are sorted in ascending order by their first matching index
+    # and then by the length of the match.
+    matches.sort(key=cmp_to_key(_compare_matches))
+    len_of_matches = [m[2] - m[1] for m in matches]
+
+    selected_matches = set()
+    i = 0
+    j = i + 1
+    while i < len(matches) - 1 and j < len(matches):
+        match_left = matches[i]
+        match_right = matches[j]
+        if len_of_matches[i] <= len_of_matches[j]:
+            if match_left[2] <= match_right[1]:
+                # [1, 2) vs [3, 5) then we can safely add [1, 2)
+                # [1, 2) vs [2, 5) we can still safely add [1. 2)
+                selected_matches.add(match_left)
+            i = j
+        else:
+            if match_left[2] <= match_right[1]:
+                # [1, 2) vs [3, 5) then we can safely add [1, 2)
+                # [1, 2) vs [2, 5) we can still safely add [1. 2)
+                selected_matches.add(match_left)
+                i = j
+        j += 1
+    selected_matches.add(matches[i])
+    return selected_matches
 
 
 class PatternModel(ITextCatModel):
@@ -95,28 +124,24 @@ class PatternModel(ITextCatModel):
         # TODO disable the right things for speed
         for doc in self.nlp.pipe(text_list, disable=["tagger", "parser"]):
             matches = self.matcher(doc)
-            matches.sort(key=cmp_to_key(_compare_matches))
+            selected_matches = _maximize_non_overlapping_matches(matches)
             # m[2] and m[1] are start and end of matches
-            len_of_matches = [m[2] - m[1] for m in matches]
-            scores = [length / len(doc) if len(doc) > 0 else 0.
-                      for length in len_of_matches]
-            scores.sort()
+            len_of_matches = [m[2] - m[1] for m in selected_matches]
+            score = sum(len_of_matches) / len(doc) if len(doc) > 0 else 0.
 
-            # Here we extract the longest match only.
-            matches = matches[-1:]
             if fancy:
                 _matches = []
-                for match_id, start, end in matches:
+                for match_id, start, end in selected_matches:
                     span = doc[start:end]
                     _matches.append((start, end, span.text))
                 res.append({
                     'tokens': [str(x) for x in list(doc)],
                     'matches': _matches,
-                    'score': scores[-1]
+                    'score': score
                 })
             else:
                 res.append({
-                    'score': scores[-1]
+                    'score': score
                 })
 
         return res
