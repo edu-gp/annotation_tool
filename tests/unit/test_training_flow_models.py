42e52b93840ce63ceaa3b65937b875c6761e4bb6
Eddie Du <sj.eddie@gmail.com>
2020-04-23 15:11:43 -0400

    Drop BackgroundJob, use more concrete tables instead

---
+++ tests/unit/test_training_flow_models.py
@@ -0,0 +1,76 @@
+from tests.sqlalchemy_conftest import *
+from db.model import (
+    EntityType, Label,
+    Task,
+    ClassificationTrainingData,
+    TextClassificationModel,
+    FileInference,
+)
+
+
+def test_flow(dbsession):
+    # Given an entity and label
+    entity_type = EntityType(name='Person')
+    label = Label(name='Smart')
+    entity_type.labels.append(label)
+
+    dbsession.add(entity_type)
+    dbsession.commit()
+    dbsession.refresh(entity_type)
+
+    # User creates a task with the label they're interested in
+    task = Task(
+        name="My Task",
+        default_params={
+            'labels': [label.name]
+        }
+    )
+
+    dbsession.add(task)
+    dbsession.commit()
+    dbsession.refresh(task)
+
+    # Pretend there are some data for this labels, we go ahead and take a
+    # snapshot and convert it to training data.
+    training_data = ClassificationTrainingData()
+    training_data.label = label
+    training_data.output_filename = '/tmp/blah.csv'
+
+    dbsession.add(training_data)
+    dbsession.commit()
+    dbsession.refresh(training_data)
+
+    assert training_data.label == label
+
+    # A model training job is kicked off. A background job runs it and in the
+    # case that it succeeds, we save the model and any post-training inferences
+    # that were completed.
+
+    model = TextClassificationModel(
+        task=task,
+        params={
+            'data_fname': training_data.output_filename,
+        }
+    )
+
+    dbsession.add(model)
+
+    inference = FileInference(
+        model=model,
+        input_filename='/my_raw_data.jsonl',
+        output_filename='/inferred_123.csv'
+    )
+
+    dbsession.add(inference)
+
+    dbsession.commit()
+    dbsession.refresh(model)
+    dbsession.refresh(inference)
+
+    # After everything, check if the relationships are right.
+    dbsession.refresh(task)
+    assert len(task.models) == 1
+    assert len(task.text_classification_models) == 1
+
+    dbsession.refresh(inference)
+    assert inference.model == model
