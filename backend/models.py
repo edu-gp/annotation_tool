4fe460932f439d3856f98457a2393e35d0a7ffdf
Eddie Du <sj.eddie@gmail.com>
2020-05-08 14:51:37 -0400

    Frontend code to export model predictions to a raw data file

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,7 @@
 from flask import (
     Blueprint, request, jsonify
 )
-from bg.jobs import export_new_raw_data
+from bg.jobs import export_new_raw_data as _export_new_raw_data
 from db.model import db, Model
 
 bp = Blueprint('models', __name__, url_prefix='/models')
@@ -9,26 +9,39 @@ bp = Blueprint('models', __name__, url_prefix='/models')
 # TODO API auth
 
 
+def get_request_data():
+    """Returns a dict of request keys and values, from either json or form"""
+    if len(request.form) > 0:
+        return request.form
+    else:
+        return request.get_json()
+
+
 @bp.route('export_new_raw_data', methods=['POST'])
-def api__export_new_raw_data():
-    data = request.get_json()
+def export_new_raw_data():
+    data = get_request_data()
 
     model_id = int(data.get('model_id'))
     data_fname = data.get('data_fname')
     output_fname = data.get('output_fname')
     cutoff = float(data.get('cutoff'))
 
-    error = None
+    resp = {
+        'error': None,
+        'message': 'Request not processed.'
+    }
+
     try:
         assert data_fname is not None, f"Missing data_fname"
         assert output_fname is not None, f"Missing output_fname"
         assert 0.0 <= cutoff < 1.0, f"Invalid cutoff={cutoff}"
 
         model = db.session.query(Model).filter_by(id=model_id).one_or_none()
-        export_new_raw_data(model, data_fname, output_fname, cutoff=cutoff)
+        output_path = _export_new_raw_data(
+            model, data_fname, output_fname, cutoff=cutoff)
+        resp['message'] = f'Successfully created raw data: {output_path}'
     except Exception as e:
-        error = str(e)
+        resp['error'] = str(e)
+        resp['message'] = f"Error: {resp['error']}"
 
-    return jsonify({
-        'error': error
-    })
+    return jsonify(resp)
