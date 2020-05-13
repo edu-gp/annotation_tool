d03532cce8a22669ebe38bc109dff16d88dd722d
edu-gp <46904813+edu-gp@users.noreply.github.com>
2020-05-13 12:45:48 -0400

    Show a page to display all the data files (#70)
    * Add a page to show the raw data
    
    * nit
    
    * Remove the "Before Request" print
    
    Co-authored-by: Eddie Du <sj.eddie@gmail.com>

---
+++ backend/data.py
@@ -0,0 +1,30 @@
+from flask import (
+    Blueprint, flash, redirect, render_template, request, url_for
+)
+
+from db.utils import get_all_data_files, get_all_pattern_files
+
+from .auth import auth
+
+bp = Blueprint('data', __name__, url_prefix='/data')
+
+
+@auth.login_required
+def _before_request():
+    """ Auth required for all routes in this module """
+    pass
+
+
+bp.before_request(_before_request)
+
+
+@bp.route('/', methods=['GET'])
+def index():
+    data_fnames = get_all_data_files()
+    pattern_fnames = get_all_pattern_files()
+
+    return render_template(
+        'data/index.html',
+        data_fnames=data_fnames,
+        pattern_fnames=pattern_fnames
+    )
