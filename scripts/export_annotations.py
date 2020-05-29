7833049b012c620aa81e87dc09b044ebbb802828
edu-gp <46904813+edu-gp@users.noreply.github.com>
2020-05-29 15:48:47 -0400

    Various bug fixes (#90)
    * Various bug fixes
    
    * Add script to export all annotations

---
+++ scripts/export_annotations.py
@@ -0,0 +1,27 @@
+from db.model import Database, ClassificationAnnotation
+from db.config import DevelopmentConfig
+import pandas as pd
+from tqdm import tqdm
+import time
+
+db = Database(DevelopmentConfig.SQLALCHEMY_DATABASE_URI)
+
+if __name__ == "__main__":
+    query = db.session.query(ClassificationAnnotation)
+
+    count = query.count()
+    print(f"Exporting {count} Annotations")
+
+    bs = 50
+
+    res = []
+    for el in tqdm(query.yield_per(bs), total=count):
+        res.append(
+            (el.id, el.entity_type, el.entity, el.user_id, el.label, el.value))
+
+    res = pd.DataFrame(res, columns=[
+        'id', 'entity_type', 'entity', 'user_id', 'label', 'value'])
+
+    now = int(time.time())
+    res.to_csv(f'/tmp/annotations_{now}.csv', index=False)
+
