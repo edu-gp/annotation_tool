b7e86055c3efe9922d12ea3eb382e39969bff648
Eddie Du <sj.eddie@gmail.com>
2020-07-15 10:39:25 -0400

    Remove gcp_celery loop
    Replace it with a job that's placed back onto queue 60 seconds later.
    
    This also cancels runaway jobs (jobs that take 1 hour or more).
    
    Finally, this includes a stricter check on AI Platform job states, so
    the worker would only reschedule itself if the job is still in progress.

--- train/gcp_celery.py
+++ train/gcp_celery.py
@@ -1,4 +1,4 @@
-import time
+import logging
 from typing import Optional
 from celery import Celery
 from train.gcp_job import GoogleAIPlatformJob, download
@@ -15,33 +15,59 @@ app = Celery(
     # backend='redis://localhost:6379/0',
 )
 
+# See all states at: https://cloud.google.com/ai-platform/training/docs/reference/rest/v1/projects.jobs#State
+RUNNING_STATES = ['QUEUED', 'PREPARING', 'RUNNING', 'CANCELLING',
+                  'STATE_UNSPECIFIED']
+TERMINAL_STATES = ['SUCCEEDED', 'FAILED', 'CANCELLED']
+
 
 @app.task
-def poll_status(job_id, metadata_dict: Optional[dict] = None):
+def poll_status(job_id: str, metadata_dict: Optional[dict] = None, poll_count: int = 0):
     """
     Inputs:
         job_id: GoogleAIPlatformJob job id
         metadata: Production metadata. If None, then this run shouldn't go into
             the production bucket.
+        poll_count: How many times did we poll already. Since we wait 60
+            seconds between polls, this tells use how long the job has been
+            running for, and we can use this to cancel runaway jobs.
     """
-    # TODO put this on a queue, rather than just a loop check.
-    while True:
-        job = GoogleAIPlatformJob.fetch(job_id)
+    logging.info(f"Poll AI Platform job_id={job_id}, poll_count={poll_count}")
 
-        if job is None:
-            raise Exception("Unknown job")
-        else:
-            if job.get_state() == 'SUCCEEDED':
-                for md in job.get_model_defns():
-                    download(md)
-                # TODO poll_status shouldn't have implementation details about callbacks
-                if metadata_dict:
-                    metadata = \
-                        DeployedInferenceMetadata.from_dict(metadata_dict)
-                    create_deployed_inference(metadata)
-                break
+    job = GoogleAIPlatformJob.fetch(job_id)
 
-        time.sleep(60)
+    if job is None:
+        raise Exception("Unknown job")
+    else:
+        job_state = job.get_state()
+        logging.info(f"job_state={job_state}")
+
+        if job_state == 'SUCCEEDED':
+            logging.info(f"Done AI Platform job_id={job_id}")
+            on_job_success(job, metadata_dict)
+        elif poll_count > 60:
+            # A job is taking too long. Try to cancel it.
+            try:
+                logging.info(f"Cancel AI Platform job_id={job_id}")
+                job.cancel()
+            except Exception:
+                # If an error occurred, we can try again later.
+                pass
+
+        if job_state in RUNNING_STATES:
+            # Check again in 60 seconds
+            poll_status.apply_async(args=[job_id, metadata_dict, poll_count+1],
+                                    countdown=60)
+
+
+def on_job_success(job: GoogleAIPlatformJob, metadata_dict: Optional[dict] = None):
+    # Sync down the model assets
+    for md in job.get_model_defns():
+        download(md)
+    # Deploy inferences to prod as needed
+    if metadata_dict:
+        metadata = DeployedInferenceMetadata.from_dict(metadata_dict)
+        create_deployed_inference(metadata)
 
 
 app.conf.task_routes = {'*.gcp_celery.*': {'queue': 'gcp_celery'}}
