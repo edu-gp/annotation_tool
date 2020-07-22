bd4ec7f6ba40e6da1693954cb203f673b652822f
Jing Zhang <jzhang@georgianpartners.com>
2020-07-22 10:40:39 -0400

    Adding the inference invoker cloud function code and requirements.txt

---
+++ cloud_function/inference/inference_api_invoker.py
@@ -0,0 +1,47 @@
+import base64
+import json
+
+from google.cloud import storage
+
+
+def hello_pubsub(event, context):
+    """Triggered from a message on a Cloud Pub/Sub topic.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+    print(pubsub_message)
+    try:
+        data_dict = json.loads(pubsub_message)
+        path = data_dict['path']
+        print(path)
+
+        storage_client = storage.Client()
+
+        source_bucket_name = path.split("/")[0]
+        source_blob_name = path[len(source_bucket_name) + 1:]
+
+        source_bucket = storage_client.bucket(source_bucket_name)
+        source_blob = source_bucket.blob(source_blob_name)
+
+        destination_bucket_name = 'alchemy-gp'
+        destination_blob_name = source_blob_name
+
+        destination_bucket = storage_client.bucket(destination_bucket_name)
+        blob_copy = source_bucket.copy_blob(
+            source_blob, destination_bucket, destination_blob_name
+        )
+
+        print(
+            "Blob {} in bucket {} copied to blob {} in bucket {}.".format(
+                source_blob.name,
+                source_bucket.name,
+                blob_copy.name,
+                destination_bucket.name,
+            )
+        )
+
+        # TODO Once the API is deployed, call the inference API.
+    except Exception as e:
+        print(e)
