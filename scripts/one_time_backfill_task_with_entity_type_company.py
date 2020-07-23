586a9fc5f4ce14bcaa6e54980ec00bfbb910c233
edu-gp <46904813+edu-gp@users.noreply.github.com>
2020-07-23 09:19:57 -0400

    update script one_time_backfill_task_with_entity_type_company.py (#121)

--- scripts/one_time_backfill_task_with_entity_type_company.py
+++ scripts/one_time_backfill_task_with_entity_type_company.py
@@ -5,14 +5,19 @@ import logging
 from db.config import DevelopmentConfig
 from db.model import Database, Task, EntityTypeEnum
 
+import datetime
+import pytz
+
 if __name__ == "__main__":
     logging.root.setLevel(logging.INFO)
 
     db = Database(DevelopmentConfig.SQLALCHEMY_DATABASE_URI)
     result = db.session.query(Task).all()
+    
+    cutoff = datetime.datetime(2020, 7, 10).replace(tzinfo=pytz.UTC)
 
     for task in result:
-        if task.get_entity_type() is None:
+        if task.created_at < cutoff:
             task.set_entity_type(EntityTypeEnum.COMPANY)
             db.session.add(task)
     db.session.commit()
