c3b7ebccd5d8c96700700ddaecfe29f80451dfcd
jzhang-gp <45179326+jzhang-gp@users.noreply.github.com>
2020-07-29 10:07:16 -0400

    Update the cloud function as we are using HTTP trigger  (#125)
    * Update the cloud function as we are using HTTP trigger instead of a cloud pubsub trigger.
    
    * fixing the unit test for the api

--- cloud_function/inference/inference_api_invoker.py
+++ cloud_function/inference/inference_api_invoker.py
@@ -6,16 +6,21 @@ import requests
 from google.cloud import storage, secretmanager
 
 
-def handler(event, context):
+def handler(request):
     """Triggered from a message on a Cloud Pub/Sub topic.
     Args:
          event (dict): Event payload.
          context (google.cloud.functions.Context): Metadata for the event.
     """
-    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
-    print(pubsub_message)
+    request_json = request.get_json()
+    message = None
+    if request.args and 'message' in request.args:
+        message = request.args.get('message')
+    elif request_json and 'message' in request_json:
+        message = request_json['message']
     try:
-        data_dict = json.loads(pubsub_message)
+        raw_data_str = base64.b64decode(message['data']).decode('utf-8')
+        data_dict = json.loads(raw_data_str)
         path = data_dict['path']
         print(path)
 
@@ -31,7 +36,7 @@ def handler(event, context):
         destination_bucket_name = os.environ.get('ALCHEMY_BUCKET')
         print("Destination bucket is " + destination_bucket_name)
         destination_file_name = "_".join(source_blob_name.split("/")[1:])
-        destination_blob_name = destination_blob_name = os.path.join(
+        destination_blob_name = os.path.join(
             'data', destination_file_name)
 
         destination_bucket = storage_client.bucket(destination_bucket_name)
