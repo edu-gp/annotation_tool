bbcc4b15f5e9e1121a906d5512b36871efe2df7f
edu-gp <46904813+edu-gp@users.noreply.github.com>
2020-07-30 14:41:00 -0400

    Rework logic of AR Examples and blacklists (#129)
    * Rework logic of AR Examples and blacklists
    
    * Nit
    
    Co-authored-by: Eddie Du <sj.eddie@gmail.com>

--- tests/unit/test_ar.py
+++ tests/unit/test_ar.py
@@ -73,66 +73,104 @@ def test_assign_blacklist():
 
 
 def populate_db(dbsession):
-    entity1 = "whatever1"
-    entity2 = "whatever2"
-    user1 = get_or_create(dbsession=dbsession,
-                          model=User,
-                          username="user1")
-    annotation0 = get_or_create(dbsession=dbsession,
-                                model=ClassificationAnnotation,
-                                value=1,
-                                entity=entity1,
-                                entity_type="company",
-                                label="b2c",
-                                user_id=user1.id)
-    annotation1 = get_or_create(dbsession=dbsession,
-                                model=ClassificationAnnotation,
-                                value=1,
-                                entity=entity1,
-                                entity_type="company",
-                                label="healthcare",
-                                user_id=user1.id)
-    user2 = get_or_create(dbsession=dbsession,
-                          model=User,
-                          username="user2")
-    annotation2 = get_or_create(dbsession=dbsession,
-                                model=ClassificationAnnotation,
-                                value=1,
-                                entity=entity2,
-                                entity_type="company",
-                                label="b2c",
-                                user_id=user2.id)
-    annotations = [annotation0, annotation1, annotation2]
-    return user1, user2, annotations, entity1, entity2
+    users = [
+        get_or_create(dbsession=dbsession, model=User, username='user0'),
+        get_or_create(dbsession=dbsession, model=User, username='user1'),
+    ]
+    entities = [
+        'entity0',
+        'entity1',
+    ]
+
+    def create_anno(user: User, entity: str, label: str, value: int):
+        instance = ClassificationAnnotation(
+            entity_type="blah",
+            entity=entity,
+            label=label,
+            value=value,
+            user_id=user.id
+        )
+        dbsession.add(instance)
+        dbsession.commit()
+
+    annotations = [
+        # Agreement
+        create_anno(users[0], entities[0], 'foo', 1),
+        create_anno(users[1], entities[0], 'foo', 1),
+
+        # Disagreement
+        create_anno(users[0], entities[1], 'foo', 1),
+        create_anno(users[1], entities[1], 'foo', -1),
+
+        # Duplicate
+        create_anno(users[0], entities[0], 'bar', 1),
+        create_anno(users[0], entities[0], 'bar', 1),
+    ]
+
+    return users, entities, annotations
 
 
-def test_build_blacklist_lookup_dict(dbsession):
-    user1, user2, annotations, _, _ = populate_db(dbsession)
-    lookup_dict = ar._build_blacklisting_lookup_dict(dbsession)
-    assert lookup_dict == {
-        ar.EntityUserTuple(annotations[0].entity, user1.username): 2,
-        ar.EntityUserTuple(annotations[2].entity, user2.username): 1,
-    }
+def test_build_blacklist_lookup(dbsession):
+    populate_db(dbsession)
+
+    def key(entity, label, user):
+        return ar.LookupKey(entity_type='blah', entity=entity, label=label, user=user)
+
+    lookup = ar._build_blacklist_lookup(dbsession, ['foo'])
+    foo_lookup = set({
+        key('entity0', 'foo', 'user0'),
+        key('entity0', 'foo', 'user1'),
+        key('entity1', 'foo', 'user0'),
+        key('entity1', 'foo', 'user1')
+    })
+    print(lookup)
+    print(foo_lookup)
+    assert lookup == foo_lookup
+
+    lookup = ar._build_blacklist_lookup(dbsession, ['bar'])
+    bar_lookup = set({
+        key('entity0', 'bar', 'user0')
+    })
+    assert lookup == bar_lookup
+
+    lookup = ar._build_blacklist_lookup(dbsession, ['foo', 'bar'])
+    assert lookup == foo_lookup.union(bar_lookup)
 
 
 def test_blacklisting_requests(dbsession):
-    user1, user2, annotations, entity1, entity2 = populate_db(dbsession)
-    dp1 = Example(score=1, entity="new_entity", fname="fname", line_number=1)
-    dp2 = Example(score=1, entity=entity1, fname="fname", line_number=1)
-    dp3 = Example(score=1, entity=entity2, fname="fname", line_number=1)
-    dp4 = Example(score=1, entity="entity_new", fname="fname", line_number=1)
-    datapoints = [dp1, dp2, dp3, dp4]
-    annotators = [user1.username, user2.username]
-    lookup_dict = ar._build_blacklisting_lookup_dict(dbsession=dbsession)
-    blacklist_fn = ar._build_blacklist_fn(lookup_dict=lookup_dict)
+    users, entities, annotations = populate_db(dbsession)
+
+    def create_dummy_example(entity, label):
+        return Example(score=1, entity_type="blah", entity=entity, label=label,
+                       fname="fname", line_number=1)
+
+    # Both users have annotated these entities with label='foo',
+    # So they should not be assigned again.
+    ex0 = create_dummy_example(entities[0], 'foo')
+    ex1 = create_dummy_example(entities[1], 'foo')
+
+    # Only users[0] has annotated entities[0] with label='bar',
+    # So it should not be assigned to users[0] again.
+    ex2 = create_dummy_example(entities[0], 'bar')
+    ex3 = create_dummy_example(entities[1], 'bar')
+
+    # New entity that has not been annotated by anyone yet.
+    ex4 = create_dummy_example('new_entity', 'foo')
+    ex5 = create_dummy_example('new_entity', 'bar')
+
+    examples = [ex0, ex1, ex2, ex3, ex4, ex5]
+
+    annotators = [user.username for user in users]
+    lookup = ar._build_blacklist_lookup(dbsession, ['foo', 'bar'])
+    blacklist_fn = ar._build_blacklist_fn(lookup)
 
     per_anno_queue = ar._assign(
-        datapoints, annotators, max_per_annotator=999, max_per_dp=999,
+        examples, annotators, max_per_annotator=999, max_per_dp=999,
         blacklist_fn=blacklist_fn)
 
     assert per_anno_queue == {
-        user1.username: [dp1, dp3, dp4],
-        user2.username: [dp1, dp2, dp4]
+        users[0].username: [ex3, ex4, ex5],
+        users[1].username: [ex2, ex3, ex4, ex5]
     }
 
 
@@ -171,14 +209,18 @@ def test_shuffle():
     def random_pred_class_a(linenum):
         return Example(
             score=0.1 + random.random()/100,
+            entity_type="blah",
             entity=str(linenum) + ".com",
+            label="foo",
             fname='data.jsonl',
             line_number=linenum)
 
     def random_pred_class_b(linenum):
         return Example(
             score=0.9 + random.random()/100,
+            entity_type="blah",
             entity=str(linenum) + ".com",
+            label="foo",
             fname='data.jsonl',
             line_number=linenum)
 
@@ -191,13 +233,17 @@ def test_shuffle_2():
     def random_pred_class_a(linenum):
         return Example(
             score=0.1,
+            entity_type="blah",
             entity=str(linenum) + ".com",
+            label="foo",
             fname='data.jsonl', line_number=linenum)
 
     def random_pred_class_b(linenum):
         return Example(
             score=0.9,
+            entity_type="blah",
             entity=str(linenum) + ".com",
+            label="foo",
             fname='data.jsonl',
             line_number=linenum)
 
@@ -208,7 +254,8 @@ def test_shuffle_2():
 
 def test__consolidate_ranked_examples_per_label__simple():
     def ex(entity):
-        return Example(score=0, entity=entity, fname='x', line_number=1)
+        return Example(score=0, entity_type="blah", entity=entity,
+                       label="foo", fname='x', line_number=1)
 
     # All labels assigned highest scores to 'c', then 'b', then 'a'.
     ranked_examples_per_label = [
@@ -223,7 +270,8 @@ def test__consolidate_ranked_examples_per_label__simple():
 
 def test__consolidate_ranked_examples_per_label__round_robin():
     def ex(entity):
-        return Example(score=0, entity=entity, fname='x', line_number=1)
+        return Example(score=0, entity_type="blah", entity=entity,
+                       label="foo", fname='x', line_number=1)
 
     # Round robin algo looks at the 1st column (a, a, c),
     # then 2nd col (b, c, a), and so on.
