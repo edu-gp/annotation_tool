be3e52c33e09e5f698b003644e3bd77b081e2e7b
Eddie Du <sj.eddie@gmail.com>
2020-09-29 12:02:17 -0400

    Address comments

--- tests/unit/test_train_no_deps_paths.py
+++ tests/unit/test_train_no_deps_paths.py
@@ -4,14 +4,14 @@ from train.no_deps.paths import (
 
 
 def test__inference_fnames_to_datasets():
-    fnames = _inference_fnames_to_datasets([
+    datasets = _inference_fnames_to_datasets([
         'blah.pred.npy',
         'blah.blah.pred.npy',
         'invalid_file.txt',  # Invalid files are ignored
         '/home/dir/ok.pred.npy']
     )
 
-    assert fnames == [
+    assert datasets == [
         'blah.jsonl',
         'blah.blah.jsonl',
         'ok.jsonl'
