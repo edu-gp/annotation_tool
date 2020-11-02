09e4cd73eb55d46c8bab86c39926971deb7e13e1
Mohammad Jafar Mashhadi <mohammad.mashhadi@georgian.io>
2020-11-02 15:14:51 -0700

    Add a TODO and a logging message. Nothing special.

--- ci/run_server.sh
+++ ci/run_server.sh
@@ -6,8 +6,10 @@ if [ -z $FLASK_ENV ]; then export FLASK_ENV=production; fi
 if [ -z $FLASK_RUN_HOST ]; then export FLASK_RUN_HOST=0.0.0.0; fi
 if [ -z $FLASK_RUN_PORT ]; then export FLASK_RUN_PORT=5000; fi
 
+# TODO: maybe do a backup before migrations if alembic is not doing transactions?
 export DB_URL_FOR_MIGRATION=$ALCHEMY_DATABASE_URI
 alembic upgrade head
 
+echo Run the development server
 # TODO: run it over uwsgi, flask run is for development only
 flask run
