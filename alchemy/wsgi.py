8e81354ec5b6d8013d306c35d84f93825136d6e1
Mohammad Jafar Mashhadi <mohammad.mashhadi@georgian.io>
2020-11-03 12:53:17 -0700

    Make wsgi more compatible with Flask's app factory specification

--- alchemy/wsgi.py
+++ alchemy/wsgi.py
@@ -5,4 +5,10 @@ from envparse import env
 app_name = env('FLASK_APP')
 app_module = importlib.import_module(app_name.replace('/', '.'))
 
-app = app_module.create_app()
+if 'create_app' in dir(app_module):
+    app = app_module.create_app()
+elif 'make_app' in dir(app_module):
+    app = app_module.make_app()
+else:
+    raise RuntimeError("Could not find the application factory in the flask app module.")
+
