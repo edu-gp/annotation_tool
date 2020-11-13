11f0e8e644130a4ad8f28d47ce03de133d0e94c9
Mohammad Jafar Mashhadi <mohammad.mashhadi@georgian.io>
2020-11-12 21:47:29 -0700

    Mock GCS

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,20 @@
-from .sqlalchemy_conftest import *
+from .sqlalchemy_conftest import *  # noqa
+
+
+@pytest.fixture(scope="session", autouse=True)
+def set_up_gcs_mock_tempdir(tmp_path_factory):
+    tmp_path = tmp_path_factory.mktemp('gcloud')
+
+    import google.cloud.storage
+
+    from .gcs_mock import Client as FakeClient
+    google.cloud.storage.__Client = google.cloud.storage.Client
+    google.cloud.storage.Client = FakeClient
+    google.cloud.storage.client.Client = FakeClient
+
+    from .gcs_mock import Blob as FakeBlob
+    google.cloud.storage.__Blob = google.cloud.storage.Blob
+    google.cloud.storage.Blob = FakeBlob
+    google.cloud.storage.blob.Blob = FakeBlob
+
+    FakeBlob.tmp_dir = tmp_path
---
+++ tests/gcs_mock.py
@@ -0,0 +1,74 @@
+import pathlib
+import tempfile
+
+
+class Blob:
+    tmp_dir = pathlib.Path(tempfile.gettempdir()) / 'test'
+
+    def __init__(self, name, bucket, **kwargs):
+        self._name = name
+        self._bucket = bucket
+        self.metadata = dict()
+
+    def _get_file(self):
+        file = Blob.tmp_dir / self._bucket.name / self._name
+        return file
+
+    def exists(self):
+        return self._get_file().exists()
+
+    def upload_from_filename(self, filename):
+        with open(filename, 'rb') as f:
+            self.upload_from_file(f)
+
+    def upload_from_file(self, file):
+        self._get_file().parent.mkdir(parents=True, exist_ok=True)
+        self._get_file().write_bytes(file.read())
+
+    def upload_from_string(self, string, encoding='utf-8'):
+        self._get_file().parent.mkdir(parents=True, exist_ok=True)
+        self._get_file().write_text(string, encoding=encoding)
+
+    def download_as_text(self, encoding='utf-8'):
+        return self._get_file().read_text(encoding=encoding)
+
+    def download_to_file(self, file):
+        file.write(self._get_file().read_bytes())
+        file.flush()
+
+    def download_to_filename(self, file_name):
+        with open(file_name, 'wb') as file:
+            return self.download_to_file(file)
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def bucket(self):
+        class Bucket:
+            def __init__(self, name):
+                self.name = name
+
+            def copy_blob(self, blob, destination_bucket, new_name):
+                src = Blob.tmp_dir / self.name / blob.name
+                dst = Blob.tmp_dir / destination_bucket.name / new_name
+                import shutil
+                shutil.copyfile(str(src), str(dst))
+
+        return Bucket(self._bucket.name)
+
+
+class Client:
+    def list_blobs(self, bucket, prefix):
+        folder = Blob.tmp_dir / bucket.name / prefix
+        if not folder.is_dir():
+            folder = folder.parent
+            prefix = prefix[prefix.rindex('/')+1:]
+        else:
+            prefix = ''
+        return [
+            Blob(f.name, bucket)
+            for f in folder.iterdir()
+            if f.name.startswith(prefix)
+        ]
