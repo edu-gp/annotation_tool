10273a25873aa7d4b8b63fc886b31bbba873da75
Mohammad Jafar Mashhadi <mohammad.mashhadi@georgian.io>
2020-11-13 17:06:14 -0700

    Removed not-needed log in paths.py

--- alchemy/train/paths.py
+++ alchemy/train/paths.py
@@ -2,8 +2,5 @@ from alchemy.db.fs import models_dir
 
 
 def get_model_dir(uuid, version):
-    # TODO: Legacy function with side effect of creating a dir
     p = models_dir(None, as_path=True) / uuid / str(version)
-    p.mkdir(parents=True, exist_ok=True)
-    print('model_dir =', p)
     return str(p)
