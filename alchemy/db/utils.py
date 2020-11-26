3a7683d1946296c4a887b8b784399be842dca6bb
Mohammad Jafar Mashhadi <mohammad.mashhadi@georgian.io>
2020-11-26 13:55:03 -0700

    Address #174#discussion_r525415436

--- alchemy/db/utils.py
+++ alchemy/db/utils.py
@@ -1,3 +1,4 @@
+import functools
 import json
 import pathlib
 
@@ -14,63 +15,42 @@ def _get_file_name(blob: storage.Blob):
     return name
 
 
-def is_data_file(fname):
+def _is_data_file_of_type(fname, file_type):
     if isinstance(fname, (str, pathlib.Path)):
         with open(fname) as f:
             try:
                 data = json.loads(f.readline())
-                return "text" in data
+                return file_type in data
             except Exception:
                 return False
     else:
         fname: storage.Blob
         if fname.metadata is None:
             return False
-        return fname.metadata.get('data_type', None) == 'data'
+        return fname.metadata.get('data_type', None) == file_type
 
 
-def is_pattern_file(fname):
-    if isinstance(fname, (str, pathlib.Path)):
-        with open(fname) as f:
-            try:
-                data = json.loads(f.readline())
-                return "pattern" in data
-            except Exception:
-                return False
+def _get_data_files_of_type(data_store, file_type):
+    d = raw_data_dir()
+    type_filter = functools.partial(_is_data_file_of_type, file_type=file_type)
+    if data_store == 'cloud':
+        client = storage.Client()
+        return sorted(list(map(
+            _get_file_name,
+            filter(type_filter,
+                   client.list_blobs(bucket_or_name=bucket_name(), prefix=d))
+        )))
+    elif data_store == 'local':
+        return sorted([x for x in listdir(d, data_store=data_store) if type_filter(d / x)])
     else:
-        fname: storage.Blob
-        if fname.metadata is None:
-            return False
-        return fname.metadata.get('data_type', None) == 'pattern'
+        raise ValueError(f"Invalid data store {data_store}")
 
 
 def get_all_data_files(data_store):
     """Return all data files in the data folder"""
-    d = raw_data_dir()
-    if data_store == 'cloud':
-        client = storage.Client()
-        return sorted(list(map(
-            _get_file_name,
-            filter(is_data_file,
-                   client.list_blobs(bucket_or_name=bucket_name(), prefix=d))
-        )))
-    elif data_store == 'local':
-        return sorted([x for x in listdir(d, data_store=data_store) if is_data_file(d / x)])
-    else:
-        raise ValueError(f"Invalid data store {data_store}")
+    return _get_data_files_of_type(data_store=data_store, file_type='text')
 
 
 def get_all_pattern_files(data_store):
     """Return all data files in the data folder"""
-    d = raw_data_dir()
-    if data_store == 'cloud':
-        client = storage.Client()
-        return sorted(list(map(
-            _get_file_name,
-            filter(is_pattern_file,
-                   client.list_blobs(bucket_or_name=bucket_name(), prefix=d))
-        )))
-    elif data_store == 'local':
-        return sorted([x for x in listdir(d, data_store=data_store) if is_pattern_file(d / x)])
-    else:
-        raise ValueError(f"Invalid data store {data_store}")
+    return _get_data_files_of_type(data_store=data_store, file_type='pattern')
